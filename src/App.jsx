111cac86c42df168498fa3a23bb58804110639d5
kdrm <kdonram@gmail.com>
2025-03-04 19:49:05 +0530

    Make some deletions and changes.

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ function App() {
 
   return (
     <>
-      <ProductCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkGMb0MAvK8P35tmFYeA56A-9R6Nd5cQNCfA&s" name="Laptop" price="$99.99" />
-      <br />
-      <ProductCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlKl28AkOioHp5Da87alIOxialChITuJC6JQ&s" name="iphone16" price="$299.99" />
       <Testing />
     </>
   )
