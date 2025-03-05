5d239912a91dde3b6dde600c9f0f7df31765932c
kdrm <kdonram@gmail.com>
2025-03-05 13:06:03 +0530

    Create a counter to count students, teachers and staffs.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
 import './counter.css';
 
+export default function Counter() {
+    const [countS, setCountS] = useState(0);
+    const [countT, setCountT] = useState(0);
+    const [countST, setCountST] = useState(0);
+    const [name, setName] = useState("Students");
 
-export default function Counter(){
-    const [count, setCount] = useState(0);
-
-    function increment(){
-        setCount(count + 1);
+    function increment(type) {
+        if (type === "Students") setCountS(countS + 1);
+        else if (type === "Teachers") setCountT(countT + 1);
+        else if (type === "Staffs") setCountST(countST + 1);
     }
 
-    function decrement(){
-        setCount(count - 1);
+    function decrement(type) {
+        if (type === "Students" && countS > 0) setCountS(countS - 1);
+        else if (type === "Teachers" && countT > 0) setCountT(countT - 1);
+        else if (type === "Staffs" && countST > 0) setCountST(countST - 1);
+    }
+
+    function changeCountType(type) {
+        setName(type);
     }
 
     return (
         <div className="background">
-            <button onClick={increment}>+</button>
-            <span>{count}</span>
-            <button onClick={decrement}>-</button>
+            <h1>{name}</h1>
+            <button onClick={() => increment(name)}>+</button>
+            <span>
+                {name === "Students" ? countS : name === "Teachers" ? countT : countST}
+            </span>
+            <button onClick={() => decrement(name)}>-</button>
+            <div className="who">
+                <button className="type" onClick={() => changeCountType("Students")}>Students</button>
+                <button className="type" onClick={() => changeCountType("Teachers")}>Teachers</button>
+                <button className="type" onClick={() => changeCountType("Staffs")}>Staffs</button>
+            </div>
         </div>
-    )    
+    );
 }
