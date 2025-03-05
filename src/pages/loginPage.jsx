75a206f95e2e32dfa1c779252d2f348fc56fcd9e
kdrm <kdonram@gmail.com>
2025-03-05 15:01:11 +0530

    Create a login page.

---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,10 @@
+export default function LoginPage(){
+    return(
+        <div>
+            <h1>Login Page</h1>
+            <input type="text" placeholder="Enter your username" />
+            <input type="password" placeholder="Enter you password" />
+            <button>login</button>
+        </div>
+    )
+}
